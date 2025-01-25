b5d7c59bd79ad81242451a0b6a4b84ce6ffc06e0
abuzeman <131666735+abuzeman@users.noreply.github.com>
2025-01-25 11:35:43 +0700

    Update install_gaia_1.sh

--- install_gaia_1.sh
+++ install_gaia_1.sh
@@ -5,19 +5,6 @@ NEON_RED='\033[38;5;196m'
 NEON_BLUE='\033[38;5;45m'
 RESET='\033[0m'
 
-# Логотип
-logo() {
-    echo -e "
-${NEON_RED}  ____   ${NEON_BLUE}____  
-${NEON_RED} |  _ \\  ${NEON_BLUE}|  _ \\ 
-${NEON_RED} | | | | ${NEON_BLUE}| |_) |
-${NEON_RED} | |_| | ${NEON_BLUE}|  __/ 
-${NEON_RED} |____/  ${NEON_BLUE}|_|    
-${RESET}
-"
-}
-
-
 # Первая часть установки Gaianet Node
 set -e
 
