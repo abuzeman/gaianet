0cc20ff8be0157997a2f18e5e24e15da71fcb91b
abuzeman <131666735+abuzeman@users.noreply.github.com>
2025-01-25 12:03:40 +0700

    Add files via upload

---
+++ faker_sessions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PYTHON_SCRIPT_NAME="request_script.py"
+FAKER_SESSION_LIMIT=5
+
+# Запуск Faker скриптов общения
+for COUNT in {1..$FAKER_SESSION_LIMIT}
+do
+    SESSION="session$COUNT"
+    screen -dmS $SESSION bash -c "python3 $PYTHON_SCRIPT_NAME"
+  #sleep $((RANDOM % (180 - 20 + 1) + 20))  # Пауза между запусками (опционально)
+    sleep 3
+done
+
+echo "Faker скрипты в количестве $FAKER_SESSION_LIMIT запущены в сессиях"
