66518f195489672a9b71ed8577e35547f382a0b1
abuzeman <131666735+abuzeman@users.noreply.github.com>
2025-01-26 20:07:56 +0700

    Update faker_sessions.sh

--- faker_sessions.sh
+++ faker_sessions.sh
@@ -5,7 +5,7 @@ FAKER_SESSION_LIMIT=5
 # Запуск Faker скриптов общения
 for COUNT in {1..$FAKER_SESSION_LIMIT}
 do
-    SESSION="session$COUNT"
+    SESSION="gaiasession$COUNT"
     screen -dmS $SESSION bash -c "python3 $PYTHON_SCRIPT_NAME"
   #sleep $((RANDOM % (180 - 20 + 1) + 20))  # Пауза между запусками (опционально)
     sleep 3
